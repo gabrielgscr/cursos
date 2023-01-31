ebf2cc0f05b656aae84e825e60900d37ceb9d401
Gabo <51181002+gabrielgscr@users.noreply.github.com>
2023-01-31 10:35:41 -0600

    Ejercicios iniciales

---
+++ Angular/ts-intro/src/ejercicios/01 index_tipos_basicos.ts
@@ -0,0 +1,12 @@
+/*
+Código de typescript
+*/
+
+let nombre :string = 'Gabriel'; //Declaración
+
+const valorConstante = 'Prueba'; //Constante
+
+let puntosVida:number | string = 95; //varios tipos de datos
+let estaVivo : boolean = true;
+
+console.log(nombre, puntosVida);
--- Angular/ts-intro/src/index.ts
+++ Angular/ts-intro/src/index.ts
@@ -1,8 +1,22 @@
-
-
-console.log('Hola Mundo!');
-
 /*
-    ===== Código de TypeScript =====
+Código de typescript
 */
 
+//let habilidades: (boolean | string | number) []= ['Bash', 'Counter', 'Healing', 100];
+
+let habilidades: string[]= ['Bash', 'Counter', 'Healing'];
+
+interface Personaje {
+    nombre: string,
+    hp:number,
+    habilidades:string[],
+    puebloNatal?:string //opcional
+}
+
+const personaje:Personaje = {
+    nombre: 'Strider',
+    hp: 100,
+    habilidades: ['Bash', 'Counter', 'Healing']
+};
+personaje.puebloNatal = 'Orosi';
+console.table(personaje);
